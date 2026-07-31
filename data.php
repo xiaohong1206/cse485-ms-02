893a4458421b5c2044c853637c3abe9ea959a324
xiaohong1206 <thuyhuong12062k4@gmail.com>
2026-07-31 09:15:50 +0700

    Create data.php

---
+++ data.php
@@ -0,0 +1,17 @@
+<?php
+$categories = [
+    ['id' => 1, 'name' => 'Ban phim', 'description' => 'Danh muc ban phim co / membrane'],
+    ['id' => 2, 'name' => 'Chuot', 'description' => 'Danh muc chuot may tinh'],
+    ['id' => 3, 'name' => 'Man hinh', 'description' => 'Danh muc man hinh'],
+];
+
+$products = [
+    ['sku' => 'KB-01', 'name' => 'Keychron K2', 'category_id' => 1, 'price' => 1890000, 'qty' => 3],
+    ['sku' => 'KB-02', 'name' => 'Akko 3087', 'category_id' => 1, 'price' => 1290000, 'qty' => 5],
+    ['sku' => 'KB-03', 'name' => 'Leopold FC660M', 'category_id' => 1, 'price' => 2750000, 'qty' => 2],
+    ['sku' => 'MS-01', 'name' => 'Logitech M331', 'category_id' => 2, 'price' => 290000, 'qty' => 10],
+    ['sku' => 'MS-02', 'name' => 'Razer Viper', 'category_id' => 2, 'price' => 990000, 'qty' => 4],
+    ['sku' => 'MS-03', 'name' => 'Xiaomi Silent', 'category_id' => 2, 'price' => 250000, 'qty' => 8],
+    ['sku' => 'MN-01', 'name' => 'Dell 24 inch', 'category_id' => 3, 'price' => 3200000, 'qty' => 2],
+    ['sku' => 'MN-02', 'name' => 'LG UltraFine', 'category_id' => 3, 'price' => 8500000, 'qty' => 1],
+];
